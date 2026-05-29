32ad0ec7a2b254fcdbda4b9af6ec665aabd1189b
Конобеев Вячеслав <slaverint@gmail.com>
2026-05-29 20:23:32 +0500

    апгрейднул версин вектор чтобы можно было извлекать измененные диапазоны

--- include/versionedVector/versioned_vector.h
+++ include/versionedVector/versioned_vector.h
@@ -2,286 +2,298 @@
 
 #include <vector>
 #include <iostream>
+#include <limits>
 
 namespace versions {
-	class Versioned {
-		unsigned int version = 0;
-
-	public:
-		void bumpVersion() {
-			version++;
-		}
-		unsigned int getVersion() const { return version; }
-	};
+    class Versioned {
+        unsigned int version = 0;
+    public:
+        void bumpVersion() { version++; }
+        unsigned int getVersion() const { return version; }
+    };
 }
 
 template<typename T, typename F>
 class Field {
-	F _func;
-	T& _value;
+    F _func;
+    T& _value;
 
 public:
-	Field(F&& func, T& value) : _func(std::forward<F>(func)), _value(value) {}
+    Field(F&& func, T& value) : _func(std::forward<F>(func)), _value(value) {}
 
-	Field& operator=(const T& new_val) {
-		this->_value = new_val;
-		_func();
-		return *this;
-	}
-	Field& operator=(T&& new_val) {
-		this->_value = new_val;
-		_func();
-		return *this;
-	}
-	bool operator==(const T& other) const {
-		return _value == other;
-	}
+    Field& operator=(const T& new_val) {
+        _value = new_val;
+        _func();
+        return *this;
+    }
+    Field& operator=(T&& new_val) {
+        _value = std::move(new_val);
+        _func();
+        return *this;
+    }
 
-	bool operator!=(const T& other) const {
-		return _value != other;
-	}
+    operator const T& () const { return _value; }
+    bool operator==(const T& data) { return data == _value; }
+    bool operator!=(const T& data) { return data != _value; }
 
-	bool operator<(const T& other) const {
-		return _value < other;
-	}
-
-	bool operator>(const T& other) const {
-		return _value > other;
-	}
-
-	bool operator<=(const T& other) const {
-		return _value <= other;
-	}
-
-	bool operator>=(const T& other) const {
-		return _value >= other;
-	}
-
-	operator const T& () const {
-		return _value;
-	}
+    // Доступ к членам, если T — структура
+    auto* operator->() { return &_value; }
+    const auto* operator->() const { return &_value; }
 };
 
 template<typename T>
 class versionedVector {
-	std::vector<T> rawVector;
-	versions::Versioned version;
+private:
+    std::vector<T> rawVector;
+    versions::Versioned version;
+
+    // Dirty range tracking
+    size_t dirty_begin = std::numeric_limits<size_t>::max();
+    size_t dirty_end = 0;
+
+    // Core dirty tracking methods
+    void mark_dirty_index(size_t index) {
+        if (index < dirty_begin) dirty_begin = index;
+        if (index + 1 > dirty_end) dirty_end = index + 1;
+        version.bumpVersion();
+    }
+
+    void mark_dirty_range(size_t begin, size_t end) {
+        if (begin < dirty_begin) dirty_begin = begin;
+        if (end > dirty_end) dirty_end = end;
+        version.bumpVersion();
+    }
+
+    void mark_all_dirty() {
+        if (!rawVector.empty()) {
+            dirty_begin = 0;
+            dirty_end = rawVector.size();
+        }
+        version.bumpVersion();
+    }
 
 public:
-	auto operator[](size_t i) {
-		auto bump = [this]() { version.bumpVersion(); };
-		return Field<T, decltype(bump)>(std::move(bump), rawVector[i]);
-	}
-	const T& operator[](size_t i) const {
-		return rawVector[i];
-	}
+    struct ranges {
+        size_t begin;
+        size_t end;
 
-	operator const std::vector<T>& () const { return rawVector; }
+        bool empty() const { return begin >= end; }
+        size_t size() const { return end - begin; }
+    };
 
-	versionedVector() = default;
+    // Dirty range API
+    ranges get_dirty_range() const {
+        if (dirty_begin == std::numeric_limits<size_t>::max()) {
+            return { 0, 0 };
+        }
+        return { dirty_begin, dirty_end };
+    }
 
-	versionedVector(size_t n)
-		: rawVector(n)
-	{
-		version.bumpVersion();
-	}
+    void clear_dirty_range() {
+        dirty_begin = std::numeric_limits<size_t>::max();
+        dirty_end = 0;
+    }
 
-	versionedVector(std::initializer_list<T> init)
-		: rawVector(init)
-	{
-		version.bumpVersion();
-	}
+    bool has_dirty_range() const {
+        return dirty_begin != std::numeric_limits<size_t>::max() && dirty_begin < dirty_end;
+    }
 
-	template<typename Container>
-	versionedVector(Container&& container)
-		: rawVector(std::forward<Container>(container))
-	{
-		version.bumpVersion();
-	}
+    auto operator[](size_t i) {
+        auto mark = [this, i]() { mark_dirty_index(i); };
+        return Field<T, decltype(mark)>(std::move(mark), rawVector[i]);
+    }
 
-	// Конструктор от итераторов
-	template<typename InputIt>
-	versionedVector(InputIt first, InputIt last)
-		: rawVector(first, last)
-	{
-		version.bumpVersion();
-	}
+    const T& operator[](size_t i) const {
+        return rawVector[i];
+    }
 
-	// Конструктор с заполнением
-	versionedVector(size_t n, const T& val)
-		: rawVector(n, val)
-	{
-		version.bumpVersion();
-	}
+    operator const std::vector<T>& () const { return rawVector; }
 
-	// Move-конструктор от std::vector
-	versionedVector(std::vector<T>&& vec) noexcept
-		: rawVector(std::move(vec))
-	{
-		version.bumpVersion();
-	}
+    versionedVector() = default;
 
-	// Копирование и перемещение
-	versionedVector(const versionedVector&) = default;
-	versionedVector(versionedVector&&) noexcept = default;
-	versionedVector& operator=(const versionedVector&) = default;
-	versionedVector& operator=(versionedVector&&) noexcept = default;
+    versionedVector(size_t n) : rawVector(n) {
+        if (n > 0) version.bumpVersion();
+    }
 
-	// Оператор присваивания от initializer_list
-	versionedVector& operator=(std::initializer_list<T> init) {
-		rawVector = init;
-		version.bumpVersion();
-		return *this;
-	}
+    versionedVector(size_t n, const T& val) : rawVector(n, val) {
+        version.bumpVersion();
+    }
 
-	// Оператор присваивания от std::vector
-	versionedVector& operator=(const std::vector<T>& vec) {
-		rawVector = vec;
-		version.bumpVersion();
-		return *this;
-	}
+    versionedVector(std::initializer_list<T> init) : rawVector(init) {
+        version.bumpVersion();
+    }
 
-	versionedVector& operator=(std::vector<T>&& vec) noexcept {
-		rawVector = std::move(vec);
-		version.bumpVersion();
-		return *this;
-	}
+    template<typename Container>
+    versionedVector(Container&& container) : rawVector(std::forward<Container>(container)) {
+        version.bumpVersion();
+    }
 
-	using iterator = std::vector<T>::iterator;
+    template<typename InputIt>
+    versionedVector(InputIt first, InputIt last) : rawVector(first, last) {
+        version.bumpVersion();
+    }
 
-	iterator begin() {
-		return rawVector.begin();
-	}
+    versionedVector(std::vector<T>&& vec) noexcept : rawVector(std::move(vec)) {
+        version.bumpVersion();
+    }
 
-	iterator end() {
-		return rawVector.end();
-	}
+    // Copy/move
+    versionedVector(const versionedVector&) = default;
+    versionedVector(versionedVector&&) noexcept = default;
+    versionedVector& operator=(const versionedVector&) = default;
+    versionedVector& operator=(versionedVector&&) noexcept = default;
 
-	using const_iterator = std::vector<T>::const_iterator;
+    // Assignment operators
+    versionedVector& operator=(std::initializer_list<T> init) {
+        rawVector = init;
+        mark_all_dirty();
+        return *this;
+    }
 
-	const_iterator begin() const {
-		return rawVector.begin();
-	}
+    versionedVector& operator=(const std::vector<T>& vec) {
+        rawVector = vec;
+        mark_all_dirty();
+        return *this;
+    }
 
-	const_iterator end() const {
-		return rawVector.end();
-	}
+    versionedVector& operator=(std::vector<T>&& vec) noexcept {
+        rawVector = std::move(vec);
+        mark_all_dirty();
+        return *this;
+    }
 
-	const_iterator cbegin() const {
-		return rawVector.cbegin();
-	}
+    // Iterators
+    using iterator = typename std::vector<T>::iterator;
+    using const_iterator = typename std::vector<T>::const_iterator;
 
-	const_iterator cend() const {
-		return rawVector.cend();
-	}
+    iterator begin() { return rawVector.begin(); }
+    iterator end() { return rawVector.end(); }
+    const_iterator begin() const { return rawVector.begin(); }
+    const_iterator end() const { return rawVector.end(); }
+    const_iterator cbegin() const { return rawVector.cbegin(); }
+    const_iterator cend() const { return rawVector.cend(); }
 
-	T& front() {
-		return rawVector.front();
-	}
+    // Element access
+    T& front() {
+        mark_dirty_index(0);
+        return rawVector.front();
+    }
+    const T& front() const { return rawVector.front(); }
 
-	const T& front() const {
-		return rawVector.front();
-	}
+    T& back() {
+        mark_dirty_index(size() - 1);
+        return rawVector.back();
+    }
+    const T& back() const { return rawVector.back(); }
 
-	T& back() {
-		return rawVector.back();
-	}
+    // Raw data access
+    T* data() { return rawVector.data(); }
+    const T* data() const { return rawVector.data(); }
 
-	const T& back() const {
-		return rawVector.back();
-	}
+    // Capacity
+    size_t size() const { return rawVector.size(); }
+    size_t capacity() const { return rawVector.capacity(); }
+    bool empty() const { return rawVector.empty(); }
+    void reserve(size_t newCapacity) {
+        rawVector.reserve(newCapacity);
+    }
 
-	auto& data() {
-		return rawVector.data();
-	}
-	const auto& data() const {
-		return rawVector.data();
-	}
+    // Resize operations
+    void resize(size_t newSize) {
+        size_t oldSize = size();
+        rawVector.resize(newSize);
+        if (newSize > oldSize) {
+            mark_dirty_range(oldSize, newSize);  // new elements
+        }
+        else if (newSize < oldSize) {
+            mark_dirty_range(0, newSize);  // existing elements potentially shifted? mark all
+        }
+    }
 
-	// методы этого класса
-	size_t getVersion() const {
-		return version.getVersion();
-	}
+    void resize(size_t newSize, const T& val) {
+        size_t oldSize = size();
+        rawVector.resize(newSize, val);
+        if (newSize > oldSize) {
+            mark_dirty_range(oldSize, newSize);
+        }
+        else if (newSize < oldSize) {
+            mark_dirty_range(0, newSize);
+        }
+    }
 
-	size_t capacity() const {
-		return rawVector.capacity();
-	}
+    // Erase operations
+    void erase(const_iterator where) {
+        size_t pos = where - rawVector.begin();
+        rawVector.erase(where);
+        mark_dirty_range(pos, size());
+    }
 
-	bool empty() const {
-		return rawVector.empty();
-	}
+    void erase(const_iterator first, const_iterator last) {
+        size_t pos = first - rawVector.begin();
+        rawVector.erase(first, last);
+        mark_dirty_range(pos, size());
+    }
 
-	// ерейзы
-	void erase(const_iterator where) {
-		rawVector.erase(where);
-	}
-	void erase(const_iterator first, const_iterator last) {
-		rawVector.erase(first, last);
-	}
+    // Insert operations
+    void insert(const_iterator where, const T& val) {
+        size_t pos = where - rawVector.begin();
+        rawVector.insert(where, val);
+        mark_dirty_range(pos, size());
+    }
 
-	// ресайзы
-	void resize(size_t newSize) {
-		rawVector.resize(newSize);
-		version.bumpVersion();
-	}
-	void resize(size_t newSize, const T& val) {
-		rawVector.resize(newSize, val);
-		version.bumpVersion();
-	}
+    void insert(const_iterator where, size_t count, const T& val) {
+        size_t pos = where - rawVector.begin();
+        rawVector.insert(where, count, val);
+        mark_dirty_range(pos, size());
+    }
 
-	// резервы
-	void reserve(size_t newCapacity) {
-		rawVector.reserve(newCapacity);
-		version.bumpVersion();
-	}
+    void insert(const_iterator where, std::initializer_list<T> list) {
+        size_t pos = where - rawVector.begin();
+        rawVector.insert(where, list);
+        mark_dirty_range(pos, size());
+    }
 
-	// Инсерты
-	void insert(const_iterator where, const T& val) {
-		rawVector.insert(where, val);
-		version.bumpVersion();
-	}
-	void insert(const_iterator where, const size_t count, const T& val) {
-		rawVector.insert(where, count, val);
-		version.bumpVersion();
-	}
-	void insert(const_iterator where, std::initializer_list<T> list) {
-		rawVector.insert(where, list);
-		version.bumpVersion();
-	}
-	template<typename Iter>
-	void insert(const_iterator where, Iter first, Iter last) {
-		rawVector.insert(where, first, last);
-		version.bumpVersion();
-	}
+    template<typename Iter>
+    void insert(const_iterator where, Iter first, Iter last) {
+        size_t pos = where - rawVector.begin();
+        rawVector.insert(where, first, last);
+        mark_dirty_range(pos, size());
+    }
 
-	void push_back(const T& val) {
-		rawVector.push_back(val);
-		version.bumpVersion();
-	}
-	void pop_back() {
-		rawVector.pop_back();
-		version.bumpVersion();
-	}
+    void push_back(const T& val) {
+        rawVector.push_back(val);
+        mark_dirty_index(size() - 1);
+    }
 
-	void clear() {
-		rawVector.clear();
-		version.bumpVersion();
-	}
+    void push_back(T&& val) {
+        rawVector.push_back(std::move(val));
+        mark_dirty_index(size() - 1);
+    }
 
-	size_t size() const {
-		return rawVector.size();
-	}
+    void pop_back() {
+        rawVector.pop_back();
+        mark_dirty_range(0, size());
+    }
 
-	auto& vec() const {
-		return rawVector;
-	}
+    void clear() {
+        rawVector.clear();
+        mark_all_dirty();
+    }
 
-	friend std::ostream& operator<<(std::ostream& os, const versionedVector& vector) {
-		os << "--- version_vector ---\n";
-		os << vector.vec() << '\n';
-		return os;
-	}
+    size_t getVersion() const { return version.getVersion(); }
+
+    auto& vec() const { return rawVector; }
+
+    friend std::ostream& operator<<(std::ostream& os, const versionedVector& vector) {
+        os << "--- versioned_vector (size=" << vector.size()
+            << ", dirty=[" << vector.dirty_begin << "," << vector.dirty_end << "]) ---\n";
+        for (const auto& val : vector.rawVector) {
+            os << val << '\n';
+        }
+        return os;
+    }
 };
+
 
 template<template<typename...> typename Vec, typename... Tags>
 class attribute_vector;
